6e258022cbf4a7783f043905adff21951c5f8524
proshik <proxoraleksandrovich@gmail.com>
2017-12-03 15:00:11 +0300

    Implementation command "translate"

--- main.go
+++ main.go
@@ -8,6 +8,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"github.com/manifoldco/promptui"
+	"errors"
+	"strconv"
+	"github.com/fatih/color"
 )
 
 func main() {
@@ -23,6 +27,142 @@ func main() {
 
 	yandexDict := NewYandex(yToken)
 	dbConnect := NewDB(dbPath)
+
+	for {
+		prompt := promptui.Select{
+			Label: "Select command",
+			Items: []string{"translate", "learn", "managment"},
+		}
+
+		_, result, err := prompt.Run()
+		if err != nil {
+			fmt.Printf("Prompt failed %v\n", err)
+			return
+		}
+
+		switch result {
+		case "translate":
+			translate(yandexDict, dbConnect)
+		case "learn":
+			learn(dbConnect)
+		case "managment":
+			managment(dbConnect)
+		}
+	}
+}
+
+func translate(yandex *YandexDict, db *DBConnect) {
+	for {
+		validate := func(input string) error {
+			if len(input) < 2 || len(input) > 25 {
+				return errors.New("From 2 to 25 symbols\n")
+			}
+			_, err := strconv.ParseFloat(input, 64)
+			if err == nil {
+				return errors.New("Not numbers\n")
+			}
+			return nil
+		}
+
+		prompt := promptui.Prompt{
+			Label:    "Word",
+			Validate: validate,
+		}
+
+		result, err := prompt.Run()
+		if err != nil {
+			fmt.Printf("Error on read word, %v\n", err)
+			return
+		}
+
+		word, err := db.GetEnv(result)
+		if err != nil {
+			fmt.Printf("Error on translate word=%s\n", result)
+		}
+
+		if word != nil {
+			printWord(word)
+			continue
+		}
+
+		fmt.Printf("Will be translate throw YandexDict\n")
+
+		tr, err := yandex.translate(result)
+		if err != nil {
+			fmt.Printf("Error on translate word=%s\n", result)
+			continue
+			//todo тут можно сделать зарпос на попытку повторного перевода
+		}
+
+		word, err = db.AddWOrd(Word{result, "Default", "Default", tr.Def})
+		if err != nil {
+			fmt.Printf("Error on save word=%s in db\n", result)
+			continue
+		}
+
+		if word.Translate == nil || len(word.Translate) == 0 {
+			fmt.Printf("Translate not found")
+			continue
+		}
+
+		printWord(word)
+	}
+
+}
+
+func printWord(word *Word) {
+	color.Red("%s\n", word.Translate[0].Tr[0].Text)
+	fmt.Printf("\n")
+
+	for _, w := range word.Translate {
+		fmt.Printf("%s [%s] %s\n", w.Text, w.Ts, w.Pos)
+		for i, t := range w.Tr {
+			fmt.Printf("%d  %s %s", i+1, t.Text, t.Gen)
+			if len(t.Syn) > 0 {
+				for _, s := range t.Syn {
+					fmt.Printf(", %s %s", s.Text, t.Gen)
+				}
+				fmt.Printf("\n")
+				if len(t.Mean) > 0 {
+					fmt.Printf("(")
+					for im, s := range t.Mean {
+						if im+1 != len(t.Mean) {
+							fmt.Printf("%s, ", s.Text)
+						} else {
+							fmt.Printf("%s", s.Text)
+						}
+					}
+					fmt.Printf(")\n")
+				}
+			} else {
+				fmt.Printf("\n")
+			}
+
+		}
+		fmt.Printf("\n")
+	}
+
+	fmt.Print("Press 'Enter' to continue...")
+	bufio.NewReader(os.Stdin).ReadBytes('\n')
+}
+
+func learn(db *DBConnect) {
+	prompt := promptui.Select{
+		Label: "Select command",
+		Items: []string{"words", "rules"},
+	}
+
+	_, result, err := prompt.Run()
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
+
+	fmt.Printf("Selected %s", result)
+
+}
+
+func managment(db *DBConnect) {
 
 }
 
