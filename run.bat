72a2fdbc15150d83191e7fa99b60204df8556817
proshik <proxoraleksandrovich@gmail.com>
2017-12-18 12:15:30 +0300

    Update run.bat file.

--- run.bat
+++ run.bat
@@ -1,3 +1,21 @@
-del goswe.exe
-go build
-goswe.exe
+@echo off
+
+set mypath=%cd%
+
+if exist goswe.exe (
+    del goswe.exe
+) else (
+    ECHO File not found for delete
+)
+
+FOR /F "tokens=* USEBACKQ" %%F IN (`go build`) DO (
+    SET var=%%F
+)
+
+if exist goswe.exe (
+    start %mypath%\goswe.exe
+) else (
+    ECHO %var%
+)
+
+
